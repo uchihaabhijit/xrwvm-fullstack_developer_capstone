ccb520086e9655754ec16e89b545447f8566c9a0
Abhijit Borkakoty <abhijit.borkakoty@hp.com>
2024-05-22 20:03:43 +0530

    linting fix

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,20 +1,22 @@
 from django.contrib import admin
 from .models import CarMake, CarModel
 
-# Register your models here. 
 
 # CarModelInline class
 class CarModelInline(admin.StackedInline):
     model = CarModel
 
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
     list_display = ['name', 'type', 'year']
-    
+
+
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
 
-# Register models here  
+
+# Register models here
 admin.site.register(CarMake, CarMakeAdmin)
 admin.site.register(CarModel, CarModelAdmin)
--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,32 +8,23 @@ from . import views
 app_name = 'djangoapp'
 urlpatterns = [
     # # path for registration
-    path(route='registration', 
-         view=views.login_user, name='registration'),
+    path(route='registration', view=views.login_user, name='registration'),
 
     # path for login
-    path(route='login', 
-         view=views.login_user, name='login'),
+    path(route='login', view=views.login_user, name='login'),
 
     # path for logout
-    path(route='logout', 
-         view=views.login_user, name='logout'),
+    path(route='logout', view=views.login_user, name='logout'),
 
     # path for dealer reviews view
-    path(route='get_dealers', 
-         view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', 
-         view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', 
-         view=views.get_dealer_details, name='dealer_details'),
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 
     # path for add a review view
-    path(route='reviews/dealer/<int:dealer_id>', 
-         view=views.get_dealer_reviews, name='dealer_details'),
-    path(route='add_review', 
-         view=views.add_review, name='add_review'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 
     # path for add cars view
-    path(route='get_cars', 
-         view=views.get_cars, name ='getcars'),
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
