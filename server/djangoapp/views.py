5001f019a900a2dde0e250d30435be116bfad9de
Abhijit Borkakoty <abhijit.borkakoty@hp.com>
2024-05-23 00:26:27 +0530

    lint fix

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -79,7 +79,8 @@ def registration(request):
         return JsonResponse(data)
 
 
-# Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+# Update the `get_dealerships` render list of dealerships 
+# all by default, particular state if state is passed
 def get_dealerships(request, state="All"):
     if state == "All":
         endpoint = "/fetchDealers"
@@ -121,7 +122,8 @@ def add_review(request):
             post_review(data)
             return JsonResponse({"status": 200})
         except Exception:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({"status": 401, 
+                                "message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 
@@ -132,5 +134,9 @@ def get_cars(request):
     if count == 0:
         initiate()
     car_models = CarModel.objects.select_related('car_make')
-    cars = [{"CarModel": car_model.name, "CarMake": car_model.car_make.name} for car_model in car_models]
+    cars = [
+        {"CarModel": car_model.name, 
+         "CarMake": car_model.car_make.name} 
+         for car_model in car_models
+    ]
     return JsonResponse({"CarModels": cars})
