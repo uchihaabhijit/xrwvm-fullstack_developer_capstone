dfef6e3268452a018aac33748c3ef64e18c7764a
Abhijit Borkakoty <abhijit.borkakoty@hp.com>
2024-05-23 00:54:41 +0530

    fix

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -110,13 +110,22 @@ AUTH_PASSWORD_VALIDATORS = [
         ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "MinimumLengthValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "CommonPasswordValidator"
+        ),
     },
     {
-        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+        "NAME": (
+            "django.contrib.auth.password_validation."
+            "NumericPasswordValidator"
+        ),
     },
 ]
 
